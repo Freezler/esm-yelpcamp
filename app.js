95ce913d8c848da25187aeff882f7474d3446708
Freezler <rldevries79@hotmail.com>
2024-12-09 19:22:43 +0100

    add: add extras to .gitignore

--- app.js
+++ app.js
@@ -5,9 +5,7 @@ import { fileURLToPath } from "url";
 import Campground from "./models/campground.js";
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-mongoose.connect("mongodb://localhost:27017/yelp-camp"), {
-
-}
+mongoose.connect("mongodb://localhost:27017/yelp-camp")
 
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
@@ -19,19 +17,18 @@ const app = express();
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
   res.render("home");
 });
 
-app.get("/makecampground", async (req, res) => {
-  const camp = new Campground({
-    title: "My Backyard", image: "https://source.unsplash.com/collection/483251", description: "cheap camping", price: 20
-  });
-  await camp.save();
-  res.send(camp);
+app.get("/campgrounds", async (req, res) => {
+  const campgrounds = await Campground.find({});
+  res.render('campgrounds/index', { campgrounds})
 });
+
+
 
 app.listen(3000, () => {
   console.log("Server started on port 3000");
