f429b7a8af71888583e844a42769cc2d03cb0901
Freezler <rldevries79@hotmail.com>
2024-12-30 11:02:27 +0100

    minor: small changes to error handling

--- app.ts
+++ app.ts
@@ -6,19 +6,18 @@ import morgan from "morgan";
 import path, { dirname } from "path";
 import { fileURLToPath } from "url";
 import { Campground } from "./models/campground.ts";
-import { detectLanguages } from "./utils/languageDetection.ts";
+import catchAsync from "./utils/catchAsync.ts";
 import { ExpressError } from './utils/ExpressError.ts';
+import { detectLanguages } from "./utils/languageDetection.ts";
 
 const app: Application = express();
 const port = process.env.PORT || 3000;
 const __dirname = dirname(fileURLToPath(import.meta.url));
-
 const myMiddleware = (req: Request, res: Response, next: NextFunction) => {
 	console.log("This is my middleware!");
 	next();
-}
+};
 
-// Apply custom middleware
 app.use(myMiddleware);
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'));
@@ -37,13 +36,12 @@ const verifyPassword = (req: Request, res: Response, next: NextFunction) => {
 	res.send('Sorry, you need a password dummy');
 }
 
-app.engine('ejs', ejsMate)
+app.engine('ejs', ejsMate);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 app.use(function (req: Request, res: Response, next: NextFunction) {
 	console.log(`${req.method} Request gemaakt vanaf ${req.protocol}//${req.hostname}:${port}${req.originalUrl} op ${new Date().toLocaleDateString('nl-NL', { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric' })} om ${new Date().toLocaleTimeString('nl-NL')}`);
-
 	return next();
 });
 
@@ -54,7 +52,7 @@ app.use(function (req: Request, res: Response, next: NextFunction) {
 // });
 // app.use((req: Request, res: Response, next: NextFunction) => {
 // console.log('My second custom middleware runs!');
-// return next(); // return prevents it from running anything else.
+
 // });
 
 app.get("/", (req: Request, res: Response) => {
@@ -67,26 +65,29 @@ app.get('/dogs', (req: Request, res: Response) => {
 
 app.get("/secret", verifyPassword, (req: Request, res: Response) => {
 	res.send('My Super secret secrets!')
-})
+});
 
 app.get("/error", (req: Request, res: Response) => {
+	console.log('Forcing an error');
 	throw new Error('This is a forced error');
+
 });
 
-app.get("/campgrounds", async (req: Request, res: Response) => {
+app.get("/campgrounds", catchAsync(async (req: Request, res: Response) => {
 	const campgrounds = await Campground.find({});
 	res.render('campgrounds/index', { campgrounds })
-});
+}));
 
 app.get("/campgrounds/new", (req: Request, res: Response) => {
 	res.render('campgrounds/new')
 });
 
-app.post('/campgrounds', async (req: Request, res: Response) => {
+app.post('/campgrounds', catchAsync(async (req: Request, res: Response) => {
+	if (!req.body.campground) throw new ExpressError('Invalid Campground Data', 400);
 	const campground = new Campground(req.body.campground);
 	await campground.save();
 	res.redirect(`campgrounds/${campground._id}`);
-});
+}));
 
 app.post('/detect-language', (req: Request, res: Response) => {
 	const input = req.body.text;
@@ -94,7 +95,7 @@ app.post('/detect-language', (req: Request, res: Response) => {
 	res.json(result);
 });
 
-app.get("/campgrounds/search", async (req: Request, res: Response) => {
+app.get("/campgrounds/search", catchAsync(async (req: Request, res: Response) => {
 	const searchQuery = req.query.searchQuery;
 	try {
 		let campgrounds;
@@ -113,19 +114,19 @@ app.get("/campgrounds/search", async (req: Request, res: Response) => {
 		console.error(e);
 		res.redirect('/campgrounds');
 	}
-});
+}));
 
-app.get("/campgrounds/:id", async (req: Request, res: Response) => {
+app.get("/campgrounds/:id", catchAsync(async (req: Request, res: Response) => {
 	const campground = await Campground.findById(req.params.id)
 	res.render('campgrounds/show', { campground });
-});
+}));
 
-app.get('/campgrounds/:id/edit', async (req: Request, res: Response) => {
+app.get('/campgrounds/:id/edit', catchAsync(async (req: Request, res: Response) => {
 	const campground = await Campground.findById(req.params.id)
 	res.render('campgrounds/edit', { campground })
-})
+}));
 
-app.put('/campgrounds/:id', async (req: Request, res: Response) => {
+app.put('/campgrounds/:id', catchAsync(async (req: Request, res: Response) => {
 	const { id } = req.params;
 	const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
 	if (campground) {
@@ -133,22 +134,28 @@ app.put('/campgrounds/:id', async (req: Request, res: Response) => {
 	} else {
 		res.status(404).send("Campground not found");
 	}
-});
+}));
 
-app.delete('/campgrounds/:id', async (req: Request, res: Response) => {
+app.delete('/campgrounds/:id', catchAsync(async (req: Request, res: Response) => {
 	const { id } = req.params;
 	await Campground.findByIdAndDelete(id);
 	res.redirect('/campgrounds');
-})
+}));
 
-app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+app.all('*', (req: Request, res: Response, next: NextFunction) => {
+	next(new ExpressError('Page not found', 404));
+});
+
+app.use((err: ExpressError, req: Request, res: Response, next: NextFunction) => {
+	const { statusCode = 500 } = err;
+	if (!err.message) err.message = 'Something went wrong';
 	console.error(`****************ERROR******************`);
-	console.error(res.statusCode, err.message, req.method, req.url, req.hostname, req.protocol, req.originalUrl);
+	console.error(`Status Code: ${statusCode}, Message: ${err.message}`);
 	console.error(`****************ERROR******************`);
-	res.status(500).send('Something broke!');
-	next(err)
+	res.status(statusCode).render('error', { err });
+
 });
 
 app.listen(port, () => {
-	console.log(`Server started on port ${port}`);
+	console.log(`Server started on port ${process.env.PORT || 3000}`);
 });
--- utils/ExpressError.ts
+++ utils/ExpressError.ts
@@ -2,5 +2,8 @@ export class ExpressError extends Error {
     constructor(message: string, public statusCode: number) {
         super(message);
         this.statusCode = statusCode;
+        this.message = message;
     }
 }
+
+export default ExpressError;
